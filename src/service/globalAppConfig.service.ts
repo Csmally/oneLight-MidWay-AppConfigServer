221dc918fc4279b5cad64fce686da49ab1ec3a38
Csmally <1130180692@qq.com>
2024-07-17 22:37:54 +0800

    feat: 格式化

--- src/service/globalAppConfig.service.ts
+++ src/service/globalAppConfig.service.ts
@@ -1,39 +1,39 @@
 import { Provide } from '@midwayjs/core';
 const homeTabs = [
-    {
-      title: '全部',
-      img: 'http://10.193.235.32:7002/public/appAssets/homePageTabs/all.png',
-      keyWord: '全部',
-      id: '0',
-    },
-    {
-      title: '美食',
-      img: 'http://10.193.235.32:7002/public/appAssets/homePageTabs/food.png',
-      keyWord: '美食',
-      id: '1',
-    },
-    {
-      title: '快递',
-      img: 'http://10.193.235.32:7002/public/appAssets/homePageTabs/express.png',
-      keyWord: '快递',
-      id: '2',
-    },
-    {
-      title: 'Replace',
-      img: 'http://10.193.235.32:7002/public/appAssets/homePageTabs/class.png',
-      keyWord: '代课',
-      id: '3',
-    },
-    {
-      title: '兼职',
-      img: 'http://10.193.235.32:7002/public/appAssets/homePageTabs/job.png',
-      keyWord: '兼职',
-      id: '4',
-    },
-  ];
+  {
+    title: '全部',
+    img: 'http://10.193.235.32:7002/public/appAssets/homePageTabs/all.png',
+    keyWord: '全部',
+    id: '0',
+  },
+  {
+    title: '美食',
+    img: 'http://10.193.235.32:7002/public/appAssets/homePageTabs/food.png',
+    keyWord: '美食',
+    id: '1',
+  },
+  {
+    title: '快递',
+    img: 'http://10.193.235.32:7002/public/appAssets/homePageTabs/express.png',
+    keyWord: '快递',
+    id: '2',
+  },
+  {
+    title: 'Replace',
+    img: 'http://10.193.235.32:7002/public/appAssets/homePageTabs/class.png',
+    keyWord: '代课',
+    id: '3',
+  },
+  {
+    title: '兼职',
+    img: 'http://10.193.235.32:7002/public/appAssets/homePageTabs/job.png',
+    keyWord: '兼职',
+    id: '4',
+  },
+];
 @Provide()
 export class GlobalAppConfigService {
   async getConfigs() {
-    return {homeTabs};
+    return { homeTabs };
   }
 }
